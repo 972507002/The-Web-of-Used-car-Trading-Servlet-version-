181f3039713931c141f5b390b114e518a3048fb6
le15874811343 <576169235@qq.com>
2016-09-11 14:21:43 +0800

    汽车图片信息操作接口

--- 前台/S4/src/cn/com/dao/ICarImagesInfoDao.java
+++ 前台/S4/src/cn/com/dao/ICarImagesInfoDao.java
@@ -4,10 +4,28 @@ import java.util.Map;
 
 import cn.com.bean.*;
 
-
+/**
+ * 汽车图片信息操作接口
+ * @author lej
+ */
 public interface ICarImagesInfoDao {
+  /**
+   * 根据编号获取汽车照片的方法
+   * @parma carInfo
+   * @return Map<Integer,String>
+   */
 public Map<Integer, String> getCarImagesInfoByID(CarInfo carInfo);
+/**
+ * 添加照片信息的方法
+ * @parma carImagesInfo
+ *@reutn int 
+ */
 public int addCarImagesInfo(CarImagesInfo carImagesInfo);
+/**
+ *修改照片信息的方法 
+ * @parmas
+ * @return int
+ */
 public int updateCarImagesInfo(CarImagesInfo carImagesInfo);
 
 }
