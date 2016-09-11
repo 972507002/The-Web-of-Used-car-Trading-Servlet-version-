21b399fc3fa29af49c1c071ef65b35522190e7b0
le15874811343 <576169235@qq.com>
2016-09-11 15:18:07 +0800

    手续信息操作接口

--- 前台/S4/src/cn/com/dao/IProcedureInfoDao.java
+++ 前台/S4/src/cn/com/dao/IProcedureInfoDao.java
@@ -4,10 +4,33 @@ import java.util.Map;
 
 import cn.com.bean.CarInfo;
 import cn.com.bean.ProcedureInfo;
-
+/**
+ * 手续信息操作接口
+ * @author lej
+ */
 public interface IProcedureInfoDao {
+  /**
+   * 
+   * 获取所有手续信息的方法
+   *@return Map<Long,ProcedureInfo> 
+   */
 public Map<Long, ProcedureInfo> getAllProcedureInfo();
+/**
+   * 
+   * 根据编号获取手续信息的方法
+   *@return ProcedureInfo
+   */
 public ProcedureInfo getProcedureInfoById(CarInfo carInfo);
+/**
+   * 
+   * 添加手续信息的方法
+   *@return int
+   */
 public int addProcedureInfo(ProcedureInfo procedureInfo);
+/**
+   * 
+   * 删除手续信息的方法
+   *@return  int
+   */
 public int updateProcedureInfo(ProcedureInfo procedureInfo);
 }
