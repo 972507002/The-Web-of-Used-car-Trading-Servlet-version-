88cf2e2c765a4bc0a7790b5f2e235431fa1b76d0
le15874811343 <576169235@qq.com>
2016-09-11 14:42:18 +0800

    车型操作接口

--- 前台/S4/src/cn/com/dao/ICarTypeDao.java
+++ 前台/S4/src/cn/com/dao/ICarTypeDao.java
@@ -2,8 +2,20 @@ package cn.com.dao;
 
 import java.util.*;
 import cn.com.bean.*;
-
+/**
+ * 车型操作接口
+ * @author
+ */
 public interface ICarTypeDao {
+	/**
+	 * 按热度获取车型信息的方法
+	 * @Map<Integer,CarType>
+	 */
 	public Map<Integer, CarType> getCarTypeByCount();
+	
+	/**
+	 * 获取所有车型信息的方法
+	 * @Map<Integer,CarType>
+	 */
 	public Map<Integer, CarType> getAllCarType();
 }
