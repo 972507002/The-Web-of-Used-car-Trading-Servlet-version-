f0cf8486890bee1a6ddb7788fce24d1bfeda78c3
le15874811343 <576169235@qq.com>
2016-09-11 15:24:12 +0800

    公司动态消息操作接口

--- 前台/S4/src/cn/com/dao/ITrendsDao.java
+++ 前台/S4/src/cn/com/dao/ITrendsDao.java
@@ -3,8 +3,21 @@ package cn.com.dao;
 import java.util.*;
 
 import cn.com.bean.*;
-
+/**
+ * 公司动态消息操作接口
+ * @author
+ */
 public interface ITrendsDao {
+       /**
+        * 获取指定条数的最新的动态消息集合
+        * @param  rows 获取记录的条数
+        * @return List<Trends> 
+        */
        public List<Trends>  getITrendsByTime(Trends trends,int rows);
+           /**
+        * 按编号获取动态消息
+        * @param  rows 获取记录的条数
+        * @return List<Trends> 
+        */
        public Trends getTrendsByWhere(Trends trends);
 }
