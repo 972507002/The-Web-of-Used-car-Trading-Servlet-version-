1f64b7932d8cefb2e10c8b867199e4f2f195e8a7
le15874811343 <576169235@qq.com>
2016-09-11 15:48:48 +0800

    汽车硬件配置信息服务接口

--- 前台/S4/src/cn/com/service/IHardwareConfigService.java
+++ 前台/S4/src/cn/com/service/IHardwareConfigService.java
@@ -4,10 +4,33 @@ import java.util.Map;
 
 import cn.com.bean.CarInfo;
 import cn.com.bean.HardwareConfig;
-
+/**
+ * 汽车硬件配置信息服务接口
+ *@author lej 
+ */
 public interface IHardwareConfigService {
+  /**
+   * 
+   * 获取所有汽车硬件配置信息的服务
+   *@return Map<Long,HardwareConfig> 
+   */
 	public Map<Long, HardwareConfig> getAllHardwareConfig();
+/**
+ * 按编号获取汽车硬件配置信息的服务
+ * @parma carInfo
+ * @return HardwareConfig 
+ */
 	public HardwareConfig getHardwareConfigById(CarInfo carInfo);
+/**
+ * 添加汽车硬件配置信息的服务
+ * @parma hardwareConfig
+ * @return boolean
+ */
 	public boolean addHardwareConfig(HardwareConfig hardwareConfig);
+/**
+ * 修改汽车硬件配置信息的服务
+ * @parma hardwareConfig
+ * @return boolean
+ */
 	public boolean updateHardwareConfig(HardwareConfig hardwareConfig);
 }
