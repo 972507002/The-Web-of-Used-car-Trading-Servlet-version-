7dfe24101426de977916e38081aa4a16a952a30c
le15874811343 <576169235@qq.com>
2016-09-11 15:52:51 +0800

    个人需求信息服务接口

--- 前台/S4/src/cn/com/service/IPersonNeedService.java
+++ 前台/S4/src/cn/com/service/IPersonNeedService.java
@@ -1,9 +1,27 @@
 package cn.com.service;
 
 import cn.com.bean.*;;
-
+/**
+ * 个人需求信息服务接口
+ *@author 
+ */
 public interface IPersonNeedService {
+  /**
+   * 
+   * 添加个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean addPersonNeed(PersonNeed personNeed);
+ /**
+   * 
+   * 删除个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean deletePersonNeed(PersonNeed personNeed);
+ /**
+   * 
+   * 获取个人需求信息的服务
+   *@return PersonNeed
+   */
 	public PersonNeed getPerSonNeed(PersonNeed personNeed);
 }
