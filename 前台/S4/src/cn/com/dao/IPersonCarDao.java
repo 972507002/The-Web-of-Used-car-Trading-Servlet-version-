e3bb6176bb34b7cd1c9673af08ea3f3be2cb13dc
le15874811343 <576169235@qq.com>
2016-09-11 15:12:29 +0800

    个人汽车订单信息操作接口

--- 前台/S4/src/cn/com/dao/IPersonCarDao.java
+++ 前台/S4/src/cn/com/dao/IPersonCarDao.java
@@ -2,11 +2,34 @@ package cn.com.dao;
 import java.util.*;
 
 import cn.com.bean.*;
-
+/**
+ * 个人汽车订单信息操作接口
+ * @author lej
+ */
 public interface IPersonCarDao {
+       /**
+        * 根据条件获取用户订单信息数量
+        * @return int
+        */
        public int getCarCountByWhere(PerSonCar perSonCar);
+       /**
+        * 根据条件获取用户订单信息集合
+        * @return  Map<Long, PerSonCar>
+        */
        public Map<Long, PerSonCar> getPerSonCarMapByWhere(PerSonCar perSonCar);
+       /**
+        *  添加用户订单信息
+        * @return int
+        */
        public int addPerSonCar(PerSonCar perSonCar);
+       /**
+        *  删除用户订单信息
+        * @return int
+        */
        public int deletePerSoncar(PerSonCar perSonCar);
+       /**
+        *  修改用户订单信息
+        * @return int
+        */
        public int updatePerSoncar(PerSonCar perSonCar,String state);
 }
