33fd8d1cd69b0a5404a44db3b7dba43ea3a292d0
le15874811343 <576169235@qq.com>
2016-09-11 13:32:59 +0800

    车辆硬件配置信息类

--- 前台/S4/src/cn/com/bean/HardwareConfig.java
+++ 前台/S4/src/cn/com/bean/HardwareConfig.java
@@ -1,23 +1,26 @@
 package cn.com.bean;
 
 import java.io.Serializable;
-
+/**
+ * 车辆硬件配置信息类
+ * @author  lej
+ */
 public class HardwareConfig implements Serializable{
- private long u_id;
- private long c_id;
- private String consoleLceScreen;
- private String autoLight;
- private String headLights;
- private String lightWash;
- private String efgv;
- private int seatNumber;
- private String fuelForm;
- private String cvt;
- private String drivingMethod;
- private String pke;
- private String keyLessgo;
-private String sunRoof;
-private String leatherSeat;
+ private long u_id; //车主编号
+ private long c_id;  //车辆编号
+ private String consoleLceScreen;  //中控台液晶屏
+ private String autoLight;  //自动头灯
+ private String headLights; //氙气大灯
+ private String lightWash;  //大灯清洗
+ private String efgv;    //电动折叠后视镜
+ private int seatNumber;  //座位数
+ private String fuelForm; //燃料形式
+ private String cvt;  //变速箱
+ private String drivingMethod;  //驱动方式
+ private String pke;  //无钥匙进入
+ private String keyLessgo; //无钥匙启动
+private String sunRoof;  //全景天窗
+private String leatherSeat;  //真皮座椅
 public long getU_id() {
 	return u_id;
 }
