e3523b96a5a888a9e6427e4048cd984d5bcd99f1
le15874811343 <576169235@qq.com>
2016-09-11 15:20:06 +0800

    销售信息操作接口

--- 前台/S4/src/cn/com/dao/ISellInfoDao.java
+++ 前台/S4/src/cn/com/dao/ISellInfoDao.java
@@ -4,30 +4,39 @@ import java.util.Map;
 
 import cn.com.bean.CarInfo;
 import cn.com.bean.SellInfo;
-
+ /**
+  * 
+  * é”€å”®ä¿¡æ¯æ“ä½œæ¥å£
+  * @author lej
+  */
 public interface ISellInfoDao {
 	/**
-	 * »ñÈ¡ËùÓĞÏúÊÛĞÅÏ¢
-	 * @return
+	 * è·å–æ‰€æœ‰é”€å”®ä¿¡æ¯
+	 * @return   Map<Long, SellInfo>
 	 */
 public Map<Long, SellInfo> getAllSellInfo();
 /**
- * ¸ù¾İ³µ±àºÅ»ñÈ¡ÏúÊÛÇé¿ö
- * @return
+ * æ ¹æ®è½¦ç¼–å·è·å–é”€å”®ä¿¡æ¯
+ * @return SellInfo
  */
 public SellInfo getSellInfoById(CarInfo carInfo);
 /**
- * Ôö¼ÓÏúÊÛÇé¿ö
+ * å¢åŠ é”€å”®ä¿¡æ¯
  * @param sellInfo
- * @return
+ * @return int
  */
 public int addSellInfo(SellInfo sellInfo);
 /**
- * É¾³ıÏúÊÛÇé¿ö
+ * åˆ é™¤é”€å”®ä¿¡æ¯
  * @param sellInfo
- * @return
+ * @return int
  */
 public int deleteSellInfo(SellInfo sellInfo);
+/**
+ * ä¿®æ”¹é”€å”®ä¿¡æ¯
+ * @param sellInfo
+ * @return int
+ */
 public int updateSellInfo(SellInfo sellInfo);
 
 }
