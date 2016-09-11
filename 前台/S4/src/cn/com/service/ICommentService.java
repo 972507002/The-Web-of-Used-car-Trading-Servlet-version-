5c979b4997c625b3a602e4785d225cd10974f5de
le15874811343 <576169235@qq.com>
2016-09-11 15:44:45 +0800

    评论服务接口

--- 前台/S4/src/cn/com/service/ICommentService.java
+++ 前台/S4/src/cn/com/service/ICommentService.java
@@ -3,10 +3,35 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.bean.Comment;
-
+/**
+ * 评论服务接口
+ * @author lej
+ */
 public interface ICommentService {
+	 /**
+           * 添加评论的服务
+           * @parma comment
+           * @return boolean
+           */
 	public boolean addComment(Comment comment) ;
+	 /**
+           * 删除评论的服务
+           * @parma comment
+           * @return boolean
+           */
 	public boolean deleteComment(Comment comment);
+	 /**
+           * 获取评论的服务
+           * @parma comment
+           * @return Comment
+           */
 	public Comment getComment(Comment comment);
+	  /**
+           * 获取两条最新的评论的服务
+           * @parma comment
+           * @parma min 最小行
+           * @parma max 最大行
+           * @return Map<Long,Comment>
+           */
 	public Map<Long, Comment> getTheTowComment(Comment comment,int min,int max);
 }
