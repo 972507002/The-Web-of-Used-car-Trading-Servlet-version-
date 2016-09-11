a7a9ea3a610ccfdb62d83aea8213e59e7eb0ccc1
le15874811343 <576169235@qq.com>
2016-09-11 15:02:14 +0800

    Update IEmissionstandardDao.java

--- 前台/S4/src/cn/com/dao/IEmissionstandardDao.java
+++ 前台/S4/src/cn/com/dao/IEmissionstandardDao.java
@@ -3,7 +3,15 @@ package cn.com.dao;
 import java.util.*;
 
 import cn.com.bean.*;
-
+/**
+ * 
+ * 排放标准操作接口
+ *@author 
+ */
 public interface IEmissionstandardDao {
+	/**
+	 * 按热度获取排放标准信息的方法
+	 * @Map<Integer,Emissionstandard>
+	 */
 	public Map<Integer,Emissionstandard> getEmissionstandardByCount();
 }
