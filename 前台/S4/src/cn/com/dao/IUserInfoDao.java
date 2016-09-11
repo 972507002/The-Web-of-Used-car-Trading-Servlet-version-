08356f8650caa34cf8f34c3460c9f36666f81af0
le15874811343 <576169235@qq.com>
2016-09-11 15:25:52 +0800

    用户信息操作接口

--- 前台/S4/src/cn/com/dao/IUserInfoDao.java
+++ 前台/S4/src/cn/com/dao/IUserInfoDao.java
@@ -2,11 +2,34 @@ package cn.com.dao;
 import java.util.Map;
 
 import cn.com.bean.*;
-
+/**
+ * 用户信息操作接口
+ * @author
+ */
 public interface IUserInfoDao {
+  /**
+   * 登录的方法
+   * @return UserInfo
+   */
   public UserInfo login(UserInfo userInfo);
+  /**
+   * 按唯一条件获取用户信息的方法
+   *@return UserInfo 
+   */
   public UserInfo getUserInfoByUnique(UserInfo userInfo);
+   /**
+   * 添加用户信息的方法
+   *@return int 
+   */
   public int addUserInfo(UserInfo userInfo);
+   /**
+   * 修改用户信息的方法
+   *@return int 
+   */
   public int updateUserInfo(UserInfo userInfo);
+   /**
+   * 修改用户密码的方法
+   *@return int 
+   */
   public int updateUserPwd(UserInfo userInfo);
 }
