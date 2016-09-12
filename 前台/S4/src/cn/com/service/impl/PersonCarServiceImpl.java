9b764264440c9183832bc4f24100153a6409a4d9
le15874811343 <576169235@qq.com>
2016-09-12 21:17:48 +0800

    个人汽车订单信息服务实现类

--- 前台/S4/src/cn/com/service/impl/PersonCarServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/PersonCarServiceImpl.java
@@ -6,14 +6,26 @@ import cn.com.bean.*;
 import cn.com.dao.IPageDao;
 import cn.com.dao.impl.*;
 import cn.com.service.*;
-
+/**
+ * 个人汽车订单信息服务实现类
+ * @author lej
+ */
 public class PersonCarServiceImpl implements IPerSonCarService,IPageDao {
+	//个人汽车订单信息操作类的引用
    private PersonCarDaoImpl personCarDao=new PersonCarDaoImpl();
+    /**
+        * 根据条件获取用户订单信息数量
+        * @return int
+        */
 	@Override
 	public int getCarCountByWhere(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
 		return personCarDao.getCarCountByWhere(perSonCar);
 	}
+	 /**
+        * 根据条件获取用户订单信息集合
+        * @return  Map<Long, PerSonCar>
+        */
 	@Override
 	public Map<Long, PerSonCar> getPerSonCarMapByWhere(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
@@ -32,17 +44,31 @@ public class PersonCarServiceImpl implements IPerSonCarService,IPageDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	/**
+	 * 获取个人汽车订单的总记录数
+	 *@return int 
+	 */
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return personCarDao.queryPersonCarCount(object);
 	}
+	/**
+	 * 分页获取个人汽车订单信息
+	 *@param curPage
+	 * @param rowsPrePage
+	 * @return Map<Long,Object>
+	 */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return personCarDao.showPersonCarList(curPage, rowsPrePage, object);
 	}
+	 /**
+        *  添加用户订单信息
+        * @return boolean
+        */
 	@Override
 	public boolean addPerSonCar(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
@@ -52,6 +78,10 @@ public class PersonCarServiceImpl implements IPerSonCarService,IPageDao {
 		}
 		return flag;
 	}
+	 /**
+        *  删除用户订单信息
+        * @return boolean
+        */
 	@Override
 	public boolean deletePerSoncar(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
@@ -61,6 +91,10 @@ public class PersonCarServiceImpl implements IPerSonCarService,IPageDao {
 		}
 		return flag;
 	}
+	 /**
+        *  修改用户订单信息
+        * @return boolean
+        */
 	@Override
 	public boolean updatePerSoncar(PerSonCar perSonCar, String state) {
 		// TODO Auto-generated method stub
