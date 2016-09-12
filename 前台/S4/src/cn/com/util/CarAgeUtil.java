42944a3d9635f5424500b7dcf88887af534702fc
le15874811343 <576169235@qq.com>
2016-09-12 21:51:04 +0800

    车龄处理工作类

--- 前台/S4/src/cn/com/util/CarAgeUtil.java
+++ 前台/S4/src/cn/com/util/CarAgeUtil.java
@@ -1,8 +1,8 @@
 package cn.com.util;
 
 public class CarAgeUtil {
-   private int minAge=0;
-   private int maxAge=0;
+   private int minAge=0;//最小车龄
+   private int maxAge=0; //最大车龄
 public int getMinAge() {
 	return minAge;
 }
@@ -15,17 +15,21 @@ public int getMaxAge() {
 public void setMaxAge(int maxAge) {
 	this.maxAge = maxAge;
 }
-public CarAgeUtil(String age){发
-	if(age.contains("ÒÔÄÚ")){
-		String [] s=age.split("Äê");
+/**
+ *处理页面传递年龄区间的方法 
+ * 
+ */
+public CarAgeUtil(String age){
+	if(age.contains("以内")){
+		String [] s=age.split("年");
 		this.maxAge=Integer.parseInt(s[0]);
 		}
-		if(age.contains("ÒÔÉÏ")){
-			String [] s=age.split("Äê");
+		if(age.contains("以上")){
+			String [] s=age.split("年");
 			this.minAge=Integer.parseInt(s[0]);
 		}
 		if(age.contains("-")){
-			String [] s=age.split("Äê");
+			String [] s=age.split("年");
 			String [] s1 =   s[0].split("-");
 			this.maxAge=Integer.parseInt(s1[1]);
 			this.minAge=Integer.parseInt(s1[0]);
