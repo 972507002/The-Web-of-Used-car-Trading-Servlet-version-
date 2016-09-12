c86712018ee1cc288ec89ee4bc4a277ab3475068
le15874811343 <576169235@qq.com>
2016-09-12 15:42:53 +0800

    评论操作实现类

--- 前台/S4/src/cn/com/dao/impl/CommentDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/CommentDaoImpl.java
@@ -12,13 +12,21 @@ import cn.com.bean.Comment;
 import cn.com.dao.ICommentDao;
 import cn.com.dao.IPageDao;
 import cn.com.util.DbUtil;
-
+/**
+ * 评论操作实现类
+ * @author lej
+ */
 public class CommentDaoImpl implements ICommentDao,IPageDao{
-
+          /**
+           * 添加评论的方法
+           * @parma comment
+           * @return int
+           */
 	@Override
 	public int addComment(Comment comment) {
 		// TODO Auto-generated method stub
 		String sql="insert into comment1 values(?,?,to_date(?,'yyyy-mm-dd HH24:mi:ss'),seq_com.nextval,null,null,null)";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(comment.getUid());
 		params.add(comment.getC_text());
@@ -26,23 +34,34 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 		
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+          /**
+           * 删除评论的方法
+           * @parma comment
+           * @return int
+           */
 	@Override
 	public int deleteComment(Comment comment) {
 		// TODO Auto-generated method stub
 		String sql="delete from comment1 where c_id=?";
+	       	//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(comment.getCid());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+          /**
+           * 获取评论的方法
+           * @parma comment
+           * @return Comment
+           */
 	@Override
 	public Comment getComment(Comment comment) {
 		// TODO Auto-generated method stub
 		Comment _comment=null;
 		String sql="select * from comment1 where c_id=?";
+		//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(comment.getCid());
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql, params);
 	try {
 		while(res.next()){
@@ -62,7 +81,13 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 		
 		return _comment;
 	}
-
+          /**
+           * 获取两条最新的评论的方法
+           * @parma comment
+           * @parma min 最小行
+           * @parma max 最大行
+           * @return Map<Long,Comment>
+           */
 	@Override
 	public Map<Long, Comment> getTheTowComment(Comment comment,int min,int max) {
 		// TODO Auto-generated method stub
@@ -72,8 +97,10 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 		sql.append("select * from(select rownum rn , b.* from(select  to_char(c_date,'yyyy-mm-dd  HH24:mi:ss') y, a.* from comment1  a ");
 		sql.append("where c_admin=? ");
 		sql.append(" order by c_date desc ) b  where rownum<="+max+") where rn>"+min+"");
+		//绑定参数
 		List<Object> parmas=new ArrayList<Object>();
 		parmas.add(comment.getC_admin());
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(),parmas );
 	try {
 		while(res.next()){
@@ -93,7 +120,7 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 	}
 	return comMap;
 	}
-
+        
 	@Override
 	public int queryMsgCount(Object object, int minPrice, int maxPrice,
 			int minDis, int maxDis, int minAge, int maxAge) {
@@ -108,7 +135,10 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+          /**
+           * 获取评论总记录条数的方法
+           * @return int
+           */
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
@@ -127,7 +157,12 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 	}
 		return count;
 	}
-
+           /**
+           * 分页获取评论信息
+           * @param curPage 当前页
+           * @param rowsPrePage 每页展示条数
+           * @return Map<Long, Object> 
+           */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
@@ -135,10 +170,8 @@ public class CommentDaoImpl implements ICommentDao,IPageDao{
 		
 		 StringBuffer sql=new StringBuffer("select * from(select rownum rn , b.* from(select  to_char(c_date,'yyyy-mm-dd HH24:mi:ss') y, a.* from comment1  a  where 1=1");
 		 Map<Long, Object> trendsMap=new HashMap<Long, Object>();
-			
-
-			
 			sql.append(" order by c_date desc ) b  where rownum<=("+curPage+")*("+rowsPrePage+")) where rn>(("+curPage+")-1)*("+rowsPrePage+")");
+			//获取结果集
 			ResultSet res=	 DbUtil.executeQuery(sql.toString(), null);
 			
 		    try {
