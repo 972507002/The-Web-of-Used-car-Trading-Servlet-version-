bfccff1d1b46bd5f4cee06a79dfeb60124f346c0
le15874811343 <576169235@qq.com>
2016-09-12 15:45:01 +0800

    行驶距离操作实现类

--- 前台/S4/src/cn/com/dao/impl/DistanceDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/DistanceDaoImpl.java
@@ -4,15 +4,23 @@ import java.util.*;
 import cn.com.bean.*;
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 行驶距离操作实现类
+ *@author lej 
+ */
 public class DistanceDaoImpl implements IDistanceDao {
-
+   /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, Distance> distanceMap=new HashMap<Integer, Distance>();
 		StringBuffer sql=new StringBuffer("select * from(select rownum rn , b.* from");
 		sql.append("(select  a.* from distance  a order by d_count desc) b  where rownum<5)");
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), null);
 	try {
 		while(res.next()){
