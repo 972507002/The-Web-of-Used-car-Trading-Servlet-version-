f63abdd357f68759d53230b11f987d31281a0759
le15874811343 <576169235@qq.com>
2016-09-12 21:12:09 +0800

    行驶距离服务实现类

--- 前台/S4/src/cn/com/service/impl/DistanceServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/DistanceServiceImpl.java
@@ -6,8 +6,18 @@ import cn.com.bean.*;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 行驶距离服务实现类
+ *@author lej 
+ */
 public class DistanceServiceImpl implements IDistanceService {
+	//行驶距离操作接口的引用
    private IDistanceDao distanceDao=new DistanceDaoImpl();
+   /**
+    * 
+    * 按热度获取行驶距离信息的服务
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
