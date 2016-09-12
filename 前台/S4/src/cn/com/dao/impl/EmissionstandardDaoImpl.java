bb4f4d3776b0744e46b601d9fe6ba085def737b5
le15874811343 <576169235@qq.com>
2016-09-12 15:46:11 +0800

    排放标准操作实现类

--- 前台/S4/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
@@ -4,14 +4,23 @@ import java.util.*;
 import cn.com.bean.*;
 import cn.com.dao.*;
 import cn.com.util.*;
+/**
+ * 
+ * 排放标准操作实现类
+ *@author  lej
+ */
 public class EmissionstandardDaoImpl implements IEmissionstandardDao {
-
+        /**
+	 * 按热度获取排放标准信息的方法
+	 * @Map<Integer,Emissionstandard>
+	 */
 	@Override
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, Emissionstandard> emissionstandardMap=new HashMap<Integer, Emissionstandard>();
 		StringBuffer sql=new StringBuffer("select * from(select rownum rn , b.* from");
 		sql.append("(select  a.* from emissionstandard  a order by e_count desc) b  where rownum<6)");
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), null);
 	try {
 		while(res.next()){
