70d9895070529e5e0797babc102b166c64d1bc57
le15874811343 <576169235@qq.com>
2016-09-12 21:33:30 +0800

    用户信息服务实现类

--- 前台/S4/src/cn/com/service/impl/UserInfoServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/UserInfoServiceImpl.java
@@ -7,8 +7,17 @@ import cn.com.dao.IPageDao;
 import cn.com.dao.IUserInfoDao;
 import cn.com.dao.impl.*;
 import cn.com.service.*;
+/**
+ * 用户信息服务实现类
+ * @author
+ */
 public class UserInfoServiceImpl implements IUserInfoService,IPageDao{
+	//用户信息操作类的引用
    private UserInfoDaoImpl userInfoDao=new UserInfoDaoImpl();
+    /**
+   * 登录的服务
+   * @return UserInfo
+   */
 	@Override
 	public UserInfo login(UserInfo userInfo) {
 		// TODO Auto-generated method stub
@@ -17,11 +26,19 @@ public class UserInfoServiceImpl implements IUserInfoService,IPageDao{
 		
 		return userInfoDao.login(userInfo);
 	}
+  /**
+   * 按唯一条件获取用户信息的服务
+   *@return UserInfo 
+   */
 	@Override
 	public UserInfo getUserInfoByUnique(UserInfo userInfo) {
 		// TODO Auto-generated method stub
 		return userInfoDao.getUserInfoByUnique(userInfo);
 	}
+  /**
+   * 添加用户信息的服务
+   *@return boolean 
+   */
 	@Override
 	public boolean addUserInfo(UserInfo userInfo) {
 		// TODO Auto-generated method stub4
@@ -31,6 +48,10 @@ public class UserInfoServiceImpl implements IUserInfoService,IPageDao{
 		}
 		return flag;
 	}
+  /**
+   * 修改用户信息的服务
+   *@return boolean 
+   */
 	@Override
 	public boolean updateUserInfo(UserInfo userInfo) {
 		// TODO Auto-generated method stub
@@ -40,6 +61,10 @@ public class UserInfoServiceImpl implements IUserInfoService,IPageDao{
 		}
 		return flag;
 	}
+  /**
+   * 修改用户密码的服务
+   *@return boolean 
+   */
 	@Override
 	public boolean updateUserPwd(UserInfo userInfo) {
 		// TODO Auto-generated method stub
@@ -62,11 +87,22 @@ public class UserInfoServiceImpl implements IUserInfoService,IPageDao{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	/**
+	 *获取用户信息总记录数
+	 * 
+	 * @return int
+	 */
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return userInfoDao.queryPersonCarCount(object);
 	}
+	/**
+	 * 分页获取用户信息
+	 * @param curPage 当前页
+	 * @param rowsPrePage 每页显示记录条数
+	 * @return Map<Long,Object>
+	 */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
