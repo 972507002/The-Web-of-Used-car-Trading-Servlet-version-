b76cf439cfc25cf987a2f96712b7507e61af9ba9
le15874811343 <576169235@qq.com>
2016-09-12 20:59:18 +0800

    汽车基本信息服务实现类

--- 前台/S4/src/cn/com/service/impl/BasicInfoServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/BasicInfoServiceImpl.java
@@ -6,22 +6,35 @@ import cn.com.bean.*;
 
 import cn.com.dao.impl.*;
 import cn.com.service.*;
-
+/**
+ * 汽车基本信息服务实现类
+ * @author lej
+ */
 public class BasicInfoServiceImpl implements IBasicInfoService{
+	//汽车基本信息操作实现类的引用
 private BasicInfoDaoImpl basicInfoDaoImpl=new BasicInfoDaoImpl();
-
+/**
+ * 获取所有基本信息的服务
+ * @return Map<Long, BasicInfo>
+*/
 @Override
 public Map<Long, BasicInfo> getAllBasic() {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasic();
 }
-
+/**
+ *根据编号获取汽车基本信息的服务
+ *@return BasicInfo
+ */
 @Override
 public BasicInfo getAllBasicById(CarInfo carInfo) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasicById(carInfo);
 }
-
+/**
+ * 添加汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean addBasicInfo(BasicInfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -31,7 +44,10 @@ public boolean addBasicInfo(BasicInfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 修改汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean updateBasicInfo(BasicInfo basicInfo) {
 	// TODO Auto-generated method stub
