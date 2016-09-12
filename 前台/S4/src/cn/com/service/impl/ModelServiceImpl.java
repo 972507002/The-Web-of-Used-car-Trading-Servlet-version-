28ec48db150b3098f539040aa1e8b107b9de1f9d
le15874811343 <576169235@qq.com>
2016-09-12 21:14:56 +0800

    车系信息服务实现类

--- 前台/S4/src/cn/com/service/impl/ModelServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/ModelServiceImpl.java
@@ -8,9 +8,18 @@ import cn.com.bean.*;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车系信息服务实现类
+ * @author lej
+ */
 public class ModelServiceImpl implements IModelService{
+	//车系信息服务接口的引用
   private IModelDao modelDao=new ModelDaoImpl();
-
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 @Override
 public List<Model> getModelByWhere(Model model) {
 	// TODO Auto-generated method stub
