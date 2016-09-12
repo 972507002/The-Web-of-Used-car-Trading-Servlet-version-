7352b8bca5d57d270f6e4406eb1135ea5ea5caef
le15874811343 <576169235@qq.com>
2016-09-12 21:22:08 +0800

    价格区间服务实现类

--- 前台/S4/src/cn/com/service/impl/PriceIntervalServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/PriceIntervalServiceImpl.java
@@ -6,9 +6,18 @@ import cn.com.bean.PriceInterval;
 import cn.com.dao.IPriceIntervalDao;
 import cn.com.dao.impl.PriceIntervalDaoImpl;
 import cn.com.service.IPriceIntervalService;
-
+/**
+ * 价格区间服务实现类
+ * @author lej
+ */
 public class PriceIntervalServiceImpl implements IPriceIntervalService{
+	//价格区间实现类的引用
    private IPriceIntervalDao intervalDao=new PriceIntervalDaoImpl();
+    /**
+   * 
+   * 按热度获取价格区间信息的服务
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, PriceInterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
