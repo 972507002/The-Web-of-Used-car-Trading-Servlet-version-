869eaad5348a2c59aee64fccd29422294b653bce
le15874811343 <576169235@qq.com>
2016-09-12 21:00:15 +0800

    车龄信息服务实现类

--- 前台/S4/src/cn/com/service/impl/CarAgeServiceImpl.java
+++ 前台/S4/src/cn/com/service/impl/CarAgeServiceImpl.java
@@ -5,8 +5,17 @@ import cn.com.bean.*;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车龄信息服务实现类
+ *@author  lej 
+ */
 public class CarAgeServiceImpl implements ICarAgeService {
+	//车龄信息操作实现类的引用
   private ICarAgeDao carAgeDao=new CarAgeDaoImpl();
+  /**
+	 * 按热度获取车龄信息的服务
+	 * @return Map<Integer,CarAge>
+	 */
 	@Override
 	public Map<Integer, CarAge> getCarAgeByCount() {
 		// TODO Auto-generated method stub
