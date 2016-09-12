26634f90c124420c6c8ae0d22a6152408d3f81d4
le15874811343 <576169235@qq.com>
2016-09-12 15:36:53 +0800

    车型操作实现类

--- 前台/S4/src/cn/com/dao/impl/CarTypeDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/CarTypeDaoImpl.java
@@ -4,8 +4,15 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
+/**
+ * 车型操作实现类
+ * @author
+ */
 public class CarTypeDaoImpl implements ICarTypeDao {
-
+        /**
+	 * 按热度获取车型信息的方法
+	 * @return Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, CarType> getCarTypeByCount() {
 		// TODO Auto-generated method stub
@@ -13,6 +20,7 @@ public class CarTypeDaoImpl implements ICarTypeDao {
 		 StringBuffer sql= new StringBuffer("select rownum,b.*  from");
 		   sql.append("(select * from cartype order by t_count desc)b ");
 		   sql.append("where rownum<7");
+		      //获取结果集
 		ResultSet res=   DbUtil.executeQuery(sql.toString(), null);
 		try {
 			while(res.next()){
@@ -31,13 +39,17 @@ public class CarTypeDaoImpl implements ICarTypeDao {
 		   return carTypeMap;
 	}
 
+	/**
+	 * 获取所有车型信息的方法
+	 * @return  Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, CarType> getAllCarType() {
 		// TODO Auto-generated method stub
 		Map<Integer, CarType> carTypeMap=new HashMap<Integer, CarType>();
 		 StringBuffer sql= new StringBuffer("select * from cartype where 1=1 ");
 		   
-		   
+		   //获取结果集
 		ResultSet res=   DbUtil.executeQuery(sql.toString(), null);
 		try {
 			while(res.next()){
