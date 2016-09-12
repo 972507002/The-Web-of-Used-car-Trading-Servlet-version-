66ac31563bf76f46bf3824b55ad5adeee9349845
le15874811343 <576169235@qq.com>
2016-09-12 16:06:49 +0800

    价格区间操作实现类

--- 前台/S4/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
@@ -5,15 +5,23 @@ import java.util.*;
 import cn.com.bean.*;
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 价格区间操作实现类
+ * @author lej
+ */
 public class PriceIntervalDaoImpl implements IPriceIntervalDao{
-
+  /**
+   * 
+   * 按热度获取价格区间信息的方法
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, PriceInterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, PriceInterval> priceIntervalMap=new HashMap<Integer, PriceInterval>();
 		StringBuffer sql=new StringBuffer("select * from(select rownum rn , b.* from");
 		sql.append("(select  a.* from priceinterval  a order by p_count desc) b  where rownum<5)");
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), null);
 	try {
 		while(res.next()){
