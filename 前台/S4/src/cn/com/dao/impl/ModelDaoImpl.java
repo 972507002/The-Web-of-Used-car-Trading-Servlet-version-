cc4b490e55e42a92c417b052a3bbc00bf1e11d70
le15874811343 <576169235@qq.com>
2016-09-12 15:49:39 +0800

    车系信息操作实现类

--- 前台/S4/src/cn/com/dao/impl/ModelDaoImpl.java
+++ 前台/S4/src/cn/com/dao/impl/ModelDaoImpl.java
@@ -4,13 +4,22 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
+/**
+ * 车系信息操作实现类
+ * @author lej
+ */
 public class ModelDaoImpl implements IModelDao {
-
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
 	@Override
 	public List<Model> getModelByWhere(Model model) {
 		// TODO Auto-generated method stub
 		List<Model> modelMap=new ArrayList<Model>();
 		StringBuffer sql=new StringBuffer("select * from model where 1=1 ");
+		//动态绑定参数和延伸SQL语句
 		List<Object> parmas=new ArrayList<Object>();
 		if(model.getB_id()!=0){
 			sql.append("and b_id=?");
@@ -24,6 +33,7 @@ public class ModelDaoImpl implements IModelDao {
 			sql.append("and m_name=?");
 			parmas.add(model.getM_name());
 		}
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), parmas);
 	try {
 		while(res.next()){
