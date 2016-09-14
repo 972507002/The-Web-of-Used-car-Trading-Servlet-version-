e5ff327e29e0c242dea6ba5796f868170da234dd
le15874811343 <576169235@qq.com>
2016-09-14 18:45:51 +0800

    个人需求信息服务接口

--- 后台/S4Manager/src/cn/com/service/IPersonNeedService.java
+++ 后台/S4Manager/src/cn/com/service/IPersonNeedService.java
@@ -2,12 +2,44 @@ package cn.com.service;
 
 import cn.com.bean.*;
 
-
+/**
+ * 个人需求信息服务接口
+ *@author 
+ */
 public interface IPersonNeedService {
+  /**
+   * 
+   * 添加个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean addPersonNeed(PersonNeed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean deletePersonNeed(PersonNeed personNeed);
+  /**
+   * 
+   * 获取个人需求信息的服务
+   *@return PersonNeed
+   */
 	public PersonNeed getPerSonNeed(PersonNeed personNeed);
+  /**
+   * 
+   * 修改个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean updatePersonNeed(PersonNeed personNeed);
+/**
+ * 
+ * 检查是否还有与某用户编号关联的个人需求的服务
+ * @return boolean
+ */
 	public boolean chekUidPerson(PersonNeed personneed);
+/**
+ * 根据用户编号删除个人需求信息的服务
+ *@return boolean  
+ */;
 	public boolean deleteUidPerson(PersonNeed personneed);
 }
