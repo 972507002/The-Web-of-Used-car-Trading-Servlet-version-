b1ccc3d65a2f8db99a61be47c4dfcc05057b40d5
le15874811343 <576169235@qq.com>
2016-09-14 21:06:52 +0800

    汽车系统配置信息服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/SystemConfigServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/SystemConfigServiceImpl.java
@@ -7,28 +7,39 @@ import cn.com.bean.SystemConfig;
 import cn.com.dao.ISystemConfigDao;
 import cn.com.dao.impl.SystemConfigDaoImpl;
 import cn.com.service.ISystemConfigService;
-
+/**
+ * 
+ * 
+ * 汽车系统配置信息服务实现类
+ * @author lej
+ */
 public class SystemConfigServiceImpl implements ISystemConfigService{
+	//汽车系统配置操作接口的引用
 	private ISystemConfigDao systemConfigDao=new SystemConfigDaoImpl();
-	/**
-	 * ��ȡ����SystemConfig��ϵ�ҵ��
-	 * @return
+        /**
+	 * 获取所有系统信息的服务
+	 * @return Map<Long, SystemConfig>
 	 */
 	@Override
 	public Map<Long, SystemConfig> getAllSystemConfig() {
 		// TODO Auto-generated method stub
 		return systemConfigDao.getAllSystemConfig();
 	}
-	/**
-	 * ͨ��u��ID��ȡSystemConfig
-	 * @param systemConfig
-	 * @return
-	 */
+/**
+ * 通过Id获取系统销售信息的服务
+ * @param carInfo
+ * @return SystemConfig
+ */
 	@Override
 	public SystemConfig getSystemConfigById(CarInfo carInfo) {
 		// TODO Auto-generated method stub
 		return systemConfigDao.getSystemConfigById(carInfo);
 	}
+/**
+ * 添加汽车系统配置信息的服务
+ * @param systemConfig
+ *@return boolean 
+ */
 	@Override
 	public boolean addSystemConfig(SystemConfig systemConfig) {
 		// TODO Auto-generated method stub
@@ -38,6 +49,11 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 		 }
 		return flag;
 	}
+/**
+ * 修改汽车系统配置信息的服务
+ * @param systemConfig
+ *@return boolean 
+ */
 	@Override
 	public boolean updateSystemConfig(SystemConfig systemConfig) {
 		// TODO Auto-generated method stub
@@ -47,6 +63,10 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 		 }
 		return flag;
 	}
+/**
+ * 根据车主编号删除汽车系统配置信息的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletesystemconfiguser(SystemConfig s) {
 		// TODO Auto-generated method stub
@@ -58,6 +78,10 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 		
 		return flag;
 	}
+/**
+ * 根据车编号删除汽车系统配置信息的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletecidsystemconfiguser(SystemConfig s) {
 		// TODO Auto-generated method stub
@@ -69,11 +93,21 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 		
 		return flag;
 	}
+/**
+ * 
+ * 检查是否还有与某车主编号关联的汽车系统配置信息的服务
+ * @return boolean
+ */
 	@Override
 	public boolean checksystemconfiguser(SystemConfig s) {
 		// TODO Auto-generated method stub
 		return systemConfigDao.checksystemconfiguser(s);
 	}
+/**
+ * 
+ * 检查是否还有与某车编号关联的汽车系统配置信息的服务
+ * @return boolean
+ */
 	@Override
 	public boolean checkcidsystemconfiguser(SystemConfig s) {
 		// TODO Auto-generated method stub
