f98fab4408d45304ff34ed2c95e65c78f032698a
le15874811343 <576169235@qq.com>
2016-09-14 13:27:11 +0800

    评论操作接口

--- 后台/S4Manager/src/cn/com/dao/ICommentDao.java
+++ 后台/S4Manager/src/cn/com/dao/ICommentDao.java
@@ -3,11 +3,41 @@ package cn.com.dao;
 import java.util.Map;
 
 import cn.com.bean.Comment;
-
+/**
+ * 评论操作接口
+ * @author lej
+ */
 public interface ICommentDao {
+           /**
+           * 添加评论的方法
+           * @parma comment
+           * @return int
+           */
           public int addComment(Comment comment);
+           /**
+           * 删除评论的方法
+           * @parma comment
+           * @return int
+           */
           public int deleteComment(Comment comment);
+           /**
+           * 获取评论的方法
+           * @parma comment
+           * @return Comment
+           */
           public Comment getComment(Comment comment);
+           /**
+           * 获取两条最新的评论的方法
+           * @parma comment
+           * @parma min 最小行
+           * @parma max 最大行
+           * @return Map<Long,Comment>
+           */
           public Map<Long, Comment> getTheTowComment(Comment  comment,int min,int max);
+           /**
+           * 修改评论的方法
+           * @parma comment
+           * @return int
+           */
         public int updateComment(Comment comment);
 }
