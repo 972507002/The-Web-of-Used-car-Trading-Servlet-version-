3790892276a11705ec8eea3004685c0a3a31e0ad
le15874811343 <576169235@qq.com>
2016-09-14 13:19:13 +0800

    汽车概要信息操作接口

--- 后台/S4Manager/src/cn/com/dao/ICarInfoDao.java
+++ 后台/S4Manager/src/cn/com/dao/ICarInfoDao.java
@@ -1,20 +1,98 @@
 package cn.com.dao;
 import java.util.*;
 import cn.com.bean.*;
+/**
+ * 汽车概要信息操作接口
+ * @author lej
+ */
 public interface ICarInfoDao {
+/**
+ * 按热度获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo> getCarInfoByCountDesc(CarInfo carInfo);
+/**
+ * 按热度和品牌获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo>  getCarInfoByBrandCountDesc(CarInfo carInfo);
+/**
+ * 按上架时间获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo> getCarInfoBySjTime(CarInfo carInfo);
+/**
+ * 获取四辆最新上架的汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo> getFourthCarInfoBySjTime(CarInfo carInfo);
+/**
+ * 按热度和车型获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
+
   public Map<Long, CarInfo>  getCarInfoByTypeCountDesc(CarInfo carInfo);
+/**
+ * 按品牌获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo> getCarInfoByBrand(CarInfo carInfo);
+/**
+ * 按车型获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long,CarInfo>   getCarInfoByType(CarInfo carInfo);
+/**
+ * 按条件获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   Map<Long, CarInfo>
+ */
   public Map<Long, CarInfo> getCarByWhere(CarInfo carInfo);
+   /**
+   * 修改汽车概要信息的方法
+   * @parma carInfo
+   * @return int
+   */
   public int updateCarInfo(CarInfo carInfo);
+  /**
+   * 添加汽车概要信息的方法
+   * @parma carInfo
+   * @return int
+   */
   public int addCarInfo(CarInfo carInfo);
+/**
+ * 按唯一条件获取汽车概要信息的方法
+ * @parma carInfo
+ * @return   CarInfo
+ */
   public CarInfo getCarInfoByUMN(CarInfo carInfo);
+   /**
+   * 修改汽车所有概要信息的方法
+   * @parma carInfo
+   * @return int
+   */
   public int updateCarAll(CarInfo carInfo);
+  /**
+ * 根据车主编号删除汽车概要信息的方法
+ *@return int  
+ */
   public int deletecarinfouser(CarInfo u);
+  /**
+ * 根据车编号删除汽车概要信息的方法
+ *@return int  
+ */
   public int deletecarinfo(CarInfo c);
+/**
+ * 
+ * 检查是否还有与某车主编号关联的汽车概要信息
+ * @return boolean
+ */
   public boolean checkcarinfouser(CarInfo c);
 }
