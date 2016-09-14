32e16b3fbe7a45b02b615e987d57f909f7122dc4
le15874811343 <576169235@qq.com>
2016-09-14 18:22:10 +0800

    车型服务接口

--- 后台/S4Manager/src/cn/com/service/ICarTypeService.java
+++ 后台/S4Manager/src/cn/com/service/ICarTypeService.java
@@ -4,12 +4,39 @@ import cn.com.bean.*;
 
 import java.util.*;
 
-
+/**
+ * 车型服务接口
+ * @author
+ */
 public interface ICarTypeService {
+	/**
+	 * 按热度获取车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public Map<Integer, CarType> getCarTypeByCount();
+	/**
+	 * 获取所有车型信息的服务
+	 * @return  Map<Integer,CarType>
+	 */
 	public Map<Integer, CarType> getAllCarType();
+/**
+ * 添加车型的服务
+ * @return int
+ */
 	public boolean addCarType(CarType carType);
+/**
+ * 删除车型的服务
+ * @return int
+ */
 	public boolean deleteCarType(CarType carType);
+/**
+ * 修改车型的服务
+ * @return int
+ */
 	public boolean updateType(CarType carType);
+/**
+ * 获取车型的服务
+ * @return CarType
+ */
 	public CarType getCarType(CarType carType);
 }
