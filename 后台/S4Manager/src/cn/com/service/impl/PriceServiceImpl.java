ad6636a877628578b10a703e75b7816464c8aa94
le15874811343 <576169235@qq.com>
2016-09-14 20:58:55 +0800

    营业额查询服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/PriceServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/PriceServiceImpl.java
@@ -5,27 +5,44 @@ import cn.com.bean.Price;
 import cn.com.dao.IPriceDao;
 import cn.com.dao.impl.PriceDaoImpl;
 import cn.com.service.IPriceService;
-
+/**
+ * 营业额查询服务实现类
+ * 
+ */
 public class PriceServiceImpl implements IPriceService {
+	//营业额查询操作实现类的引用
 private IPriceDao priceDao=new PriceDaoImpl();
+        /**
+	 * 按年月日查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month, day);
 	}
-
+        /**
+	 * 按年月查询的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo(int year, int month) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month);
 	}
-
+        /**
+	 * 按年查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year);
 	}
-
+        /**
+	 * 查询不同年营业额的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo() {
 		// TODO Auto-generated method stub
