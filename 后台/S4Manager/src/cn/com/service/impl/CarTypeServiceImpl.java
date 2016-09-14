da560bcc5acb28cef8efde8e470a58235bb7bb83
le15874811343 <576169235@qq.com>
2016-09-14 19:47:05 +0800

    车型服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/CarTypeServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/CarTypeServiceImpl.java
@@ -6,23 +6,37 @@ import cn.com.bean.*;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车型服务实现类
+ * @author
+ */
 public class CarTypeServiceImpl  implements ICarTypeService,IPageDao{
+	//车型操作实现类的引用
    private CarTypeDaoImpl carTypeDao=new CarTypeDaoImpl();
 
-
+        /**
+	 * 按热度获取车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 @Override
 public Map<Integer, CarType> getCarTypeByCount() {
 	// TODO Auto-generated method stub
 	return carTypeDao.getCarTypeByCount();
 }
 
-
+        /**
+	 * 获取所有车型信息的服务
+	 * @return  Map<Integer,CarType>
+	 */
 @Override
 public Map<Integer, CarType> getAllCarType() {
 	// TODO Auto-generated method stub
 	return carTypeDao.getAllCarType();
 }
-
+/**
+ * 添加车型的服务
+ * @return boolean
+ */
 @Override
 public boolean addCarType(CarType carType) {
 	// TODO Auto-generated method stub
@@ -32,7 +46,10 @@ public boolean addCarType(CarType carType) {
 	}
 	return flag;
 }
-
+/**
+ * 删除车型的服务
+ * @return boolean
+ */
 @Override
 public boolean deleteCarType(CarType carType) {
 	// TODO Auto-generated method stub
@@ -42,7 +59,10 @@ public boolean deleteCarType(CarType carType) {
 	}
 	return flag;
 }
-
+/**
+ * 修改车型的服务
+ * @return boolean
+ */
 @Override
 public boolean updateType(CarType carType) {
 	// TODO Auto-generated method stub
@@ -53,7 +73,10 @@ public boolean updateType(CarType carType) {
 	return flag;
 }
 
-
+/**
+ * 获取车型的服务
+ * @return CarType
+ */
 @Override
 public int queryMsgCount(Object object, int minPrice, int maxPrice, int minDis,
 		int maxDis, int minAge, int maxAge) {
@@ -70,14 +93,22 @@ public List<Object> showMsgInfoList(int curPage, int rowsPrePage,
 	return null;
 }
 
-
+    /**
+     * 获取车型信息的记录总条数的服务
+     * @return int
+     */	
 @Override
 public int queryPersonCarCount(Object object) {
 	// TODO Auto-generated method stub
 	return carTypeDao.queryPersonCarCount(object);
 }
 
-
+/**
+ * 分页获取车型信息的服务
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 @Override
 public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 		Object object) {
@@ -85,7 +116,10 @@ public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 	return carTypeDao.showPersonCarList(curPage, rowsPrePage, object);
 }
 
-
+/**
+ * 获取车型的服务
+ * @return CarType
+ */
 @Override
 public CarType getCarType(CarType carType) {
 	// TODO Auto-generated method stub
