004e2a5393d3f3ed69a94a40d8dc75a29c53060a
le15874811343 <576169235@qq.com>
2016-09-14 15:48:02 +0800

    车系信息操作实现类

--- 后台/S4Manager/src/cn/com/dao/impl/ModelDaoImpl.java
+++ 后台/S4Manager/src/cn/com/dao/impl/ModelDaoImpl.java
@@ -4,14 +4,23 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
+/**
+ * 车系信息操作实现类
+ * @author lej
+ */
 public class ModelDaoImpl implements IModelDao {
-
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
 	@Override
 	public List<Model> getModelByWhere(Model model) {
 		// TODO Auto-generated method stub
 		List<Model> modelMap=new ArrayList<Model>();
 		StringBuffer sql=new StringBuffer("select * from model where 1=1 ");
 		List<Object> parmas=new ArrayList<Object>();
+		//动态绑定参数并延伸SQL语句
 		if(model.getB_id()!=0){
 			sql.append("and b_id=?");
 			parmas.add(model.getB_id());
@@ -24,6 +33,7 @@ public class ModelDaoImpl implements IModelDao {
 			sql.append("and m_name=?");
 			parmas.add(model.getM_name());
 		}
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), parmas);
 	try {
 		while(res.next()){
@@ -39,32 +49,47 @@ public class ModelDaoImpl implements IModelDao {
 	}
 	return modelMap;
 	}
-
+      /**
+       * 添加车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int addModel(Model model) {
 		// TODO Auto-generated method stub
 		String sql="insert into model values(seq_model.nextval,?,?)";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(model.getB_id());
 		params.add(model.getM_name());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+      /**
+       * 修改车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int updateModel(Model model) {
 		// TODO Auto-generated method stub
 		String sql="update model set b_id=? ,m_name=? where m_id=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(model.getB_id());
 		params.add(model.getM_name());
 		params.add(model.getM_id());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+      /**
+       * 删除车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int deleteModel(Model model) {
 		// TODO Auto-generated method stub
 		String sql="delete from model where m_id=?";
+		//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(model.getM_id());
 		return DbUtil.executeUpdate(sql, params);
