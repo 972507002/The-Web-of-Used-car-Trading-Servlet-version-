0e3b7787390194085331ff339b040d796bb2f8f9
le15874811343 <576169235@qq.com>
2016-09-14 17:59:46 +0800

    车龄信息服务接口

--- 后台/S4Manager/src/cn/com/service/ICarAgeService.java
+++ 后台/S4Manager/src/cn/com/service/ICarAgeService.java
@@ -2,11 +2,34 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.bean.*;
-
+/**
+ * 车龄信息服务接口
+ *@author  lej 
+ */
 public interface ICarAgeService {
+	/**
+	 * 按热度获取车龄信息的服务
+	 * @return Map<Integer,CarAge>
+	 */
 	public Map<Integer,CarAge> getCarAgeByCount();
+	/**
+	 * 添加车龄信息的服务
+	 * @return   int
+	 */
 	public boolean addCarAge(CarAge carAge);
+	/**
+	 * 删除车龄信息的服务
+	 * @return   int
+	 */
 	public boolean deleteCarAge(CarAge carAge);
+	/**
+	 * 修改车龄信息的服务
+	 * @return   int
+	 */
 	public boolean updateCarAge(CarAge carAge);
+	/**
+	 * 获取车龄信息的服务
+	 * @return CarAge
+	 */
 	public CarAge getCarAgeById(CarAge carAge);
 }
