5b44195ad4da989c790b2160b7dd33c0f879f991
le15874811343 <576169235@qq.com>
2016-09-14 19:38:16 +0800

    品牌信息服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/CarBrandServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/CarBrandServiceImpl.java
@@ -5,21 +5,36 @@ import cn.com.bean.CarBrand;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 品牌信息服务实现类
+ * @author lej
+ */
 public class CarBrandServiceImpl implements ICarBrandService,IPageDao {
+	//品牌信息操作实现类的引用
   private CarBrandDaoImpl brandDao= new CarBrandDaoImpl();
-
+  /**
+   * 按热度获取品牌信息的服务
+   * @return  Map<Integer, CarBrand>
+   */
 	@Override
 	public Map<Integer, CarBrand> getCarBrandByCount() {
 		// TODO Auto-generated method stub
 		return brandDao.getCarBrandByCount();
 	}
-	
+  /**
+   * 获取所有品牌信息的服务
+   * @return Map<Integer,CarBrand>
+   */
 	@Override
 	public Map<Integer, CarBrand> getAllBrand() {
 		// TODO Auto-generated method stub
 		return brandDao.getAllBrand();
 	}
-	
+  /**
+   * 根据品牌编号获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	@Override
 	public CarBrand getBrandByID(CarBrand carBrand) {
 		// TODO Auto-generated method stub
@@ -40,19 +55,31 @@ public class CarBrandServiceImpl implements ICarBrandService,IPageDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+    /**
+     * 获取品牌信息的记录总条数的服务
+     * @return int
+     */	
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return brandDao.queryPersonCarCount(object);
 	}
-	
+ /**
+ * 分页获取品牌信息的服务
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return brandDao.showPersonCarList(curPage, rowsPrePage, object);
 	}
+  /**
+   * 添加品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean addCarBrand(CarBrand carBrand) {
 		// TODO Auto-generated method stub
@@ -62,6 +89,10 @@ public class CarBrandServiceImpl implements ICarBrandService,IPageDao {
 		}
 		return flag;
 	}
+  /**
+   * 修改品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean updateCarBrand(CarBrand carBrand) {
 		// TODO Auto-generated method stub
@@ -71,6 +102,10 @@ public class CarBrandServiceImpl implements ICarBrandService,IPageDao {
 		}
 		return flag;
 	}
+  /**
+   * 删除品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean deleteCarBrand(CarBrand carBrand) {
 		// TODO Auto-generated method stub
