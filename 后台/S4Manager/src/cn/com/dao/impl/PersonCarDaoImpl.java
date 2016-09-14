f69212f8571160fc723d3fe98134300380714039
le15874811343 <576169235@qq.com>
2016-09-14 15:57:42 +0800

    个人汽车订单信息操作实现类

--- 后台/S4Manager/src/cn/com/dao/impl/PersonCarDaoImpl.java
+++ 后台/S4Manager/src/cn/com/dao/impl/PersonCarDaoImpl.java
@@ -4,8 +4,17 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
+/**
+ * 个人汽车订单信息操作实现类
+ * @author lej
+ */
 public class PersonCarDaoImpl implements IPersonCarDao,IPageDao {
+	//汽车概要信息操作接口的引用
     private ICarInfoDao carInfoDao=new CarInfoDaoImpl();
+       /**
+        * 根据条件获取用户订单信息数量
+        * @return int
+        */
 	@Override
 	public int getCarCountByWhere(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
@@ -31,7 +40,10 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		}
 		return count;
 	}
-
+      /**
+        * 根据条件获取用户订单信息集合
+        * @return  Map<Long, PerSonCar>
+        */
 	@Override
 	public Map<Long, PerSonCar> getPerSonCarMapByWhere(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
@@ -86,14 +98,22 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+        /**
+        * 根据条件获取用户订单信息数量
+        * @return int
+        */
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
-		PerSonCar perSonCar=(PerSonCar) object;
-		return getCarCountByWhere(perSonCar);
+		PerSonCar perSonCar=(PerSonCar) object;  //强制转型
+		return getCarCountByWhere(perSonCar);     //调用getCarCountByWhere(perSonCar) 方法
 	}
-
+        /**
+	 * 根据条件分页获取用户订车详情集合
+	 * @param curPage 当前页
+	 * @param rowsPrePage 每页面记录数
+	 * @return Map<Long,Object>
+	 */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
@@ -101,7 +121,8 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		// TODO Auto-generated method stub
 		PerSonCar perSonCar=(PerSonCar) object;
 		Map<Long, Object> carMap=new HashMap<Long, Object>();
-   Map<Long, PerSonCar>	perSonMap=	getPersonList(curPage,rowsPrePage,perSonCar);
+   Map<Long, PerSonCar>	perSonMap=	getPersonList(curPage,rowsPrePage,perSonCar);   //获取个人汽车订单集合
+    //获取该用户所订车的详细信息
    for(Long key:perSonMap.keySet()){
 	   CarInfo carInfo=new CarInfo();
 	   carInfo.setC_id(perSonMap.get(key).getC_id());
@@ -110,7 +131,12 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
    }
 		return carMap;
 	}
-
+        /**
+	 * 根据条件分页获取记录集合
+	 * @param curPage 当前页
+	 * @param rowsPrePage 每页面记录数
+	 * @return Map<Long,PerSonCar>
+	 */
 	@Override
 	public Map<Long, PerSonCar> getPersonList(int curPage, int rowsPrePage,PerSonCar perSonCar){
 		 StringBuffer sql=new StringBuffer("select * from (select rownum rn , b. * from (select rownum  ,a.* from  (select * from personcar where u_id<>c_uid");
@@ -126,7 +152,8 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 				params.add(perSonCar.getP_state());
 			}
 			sql.append(") a where rownum<=("+curPage+")*("+rowsPrePage+")) b) where rn>(("+curPage+")-1)*("+rowsPrePage+")");
-			ResultSet res=	 DbUtil.executeQuery(sql.toString(), params);
+		         //获取结果集
+		      	ResultSet res=	 DbUtil.executeQuery(sql.toString(), params);
 		    try {
 				while(res.next()){
 					PerSonCar _PerSonCar=new PerSonCar();
@@ -144,11 +171,15 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		  
 			return perSonMap;
 	}
-
+       /**
+        *  添加用户订单信息
+        * @return int
+        */
 	@Override
 	public int addPerSonCar(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
 		StringBuffer sql=new StringBuffer("insert into personcar(u_id,c_uid,c_id,p_state,p_id) values(?,?,?,?,seq_personcar.nextval)");
+		 	//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(perSonCar.getU_id());
 		params.add(perSonCar.getC_uid());
@@ -156,12 +187,16 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		params.add(perSonCar.getP_state());
 		return DbUtil.executeUpdate(sql.toString(), params);
 	}
-
+       /**
+        *  删除用户订单信息
+        * @return int
+        */
 	@Override
 	public int deletePerSoncar(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
 		StringBuffer sql=new StringBuffer("delete from personcar where u_id=? and c_id=? and c_uid=? and p_state=?");
-		List<Object> params=new ArrayList<Object>();
+	        	//绑定参数
+	    	List<Object> params=new ArrayList<Object>();
 		params.add(perSonCar.getU_id());
 		params.add(perSonCar.getC_id());
 		params.add(perSonCar.getC_uid());
@@ -169,11 +204,15 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		params.add(perSonCar.getP_state());
 		return DbUtil.executeUpdate(sql.toString(), params);
 	}
-
+       /**
+        *  修改用户订单信息
+        * @return int
+        */
 	@Override
 	public int updatePerSoncar(PerSonCar perSonCar,String state) {
 		// TODO Auto-generated method stub
 		StringBuffer sql=new StringBuffer("update  personcar set  p_state=? where u_id=? and c_id=? and c_uid=? and p_state=?");
+	      	//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(state);
 		params.add(perSonCar.getU_id());
@@ -183,37 +222,52 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		params.add(perSonCar.getP_state());
 		return DbUtil.executeUpdate(sql.toString(), params);
 	}
-
+/**
+ * 根据用户编号删除用户订单信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletepersoncaruser(PerSonCar u) {
 		// TODO Auto-generated method stub
 		String sql="delete from personcar where u_id=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(u.getU_id());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+/**
+ * 根据车编号删除用户订单信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletecidpersoncaruser(PerSonCar u) {
 		// TODO Auto-generated method stub
 		String sql="delete from personcar where c_id=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(u.getC_id());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+/**
+ * 
+ * 检查是否还有与某用户编号关联的用户订单信息
+ * @return boolean
+ */
 	@Override
 	public boolean checkipersonuser(PerSonCar p) {
 		// TODO Auto-generated method stub
 		boolean flag=false;
 		String sql="select * from personcar where u_id=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
-		params.add(p.getU_id());
+		params.add(p.getU_id()); 
+		//获取结果集
 		ResultSet res=DbUtil.executeQuery(sql, params);
 		try {
 			while(res.next())
 			{
-				flag=true;
+				flag=true;  //若结果集存在，则返回为真
+				break; //跳出循环
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -221,19 +275,26 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		}
 		return flag;
 	}
-
+ /**
+ * 
+ * 检查是否还有与某车编号关联的用户订单信息
+ * @return boolean
+ */
 	@Override
 	public boolean checkcidipersonuser(PerSonCar p) {
 		// TODO Auto-generated method stub
 		boolean flag=false;
 		String sql="select * from personcar where c_id=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(p.getC_id());
+		//获取结果集
 		ResultSet res=DbUtil.executeQuery(sql, params);
 		try {
 			while(res.next())
 			{
-				flag=true;
+				flag=true;  //若结果集存在，则返回为真
+				break; //跳出循环
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -241,11 +302,15 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		}
 		return flag;
 	}
-
+ /**
+ * 根据所有字段删除用户订单信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletePersonByAll(PerSonCar perSonCar) {
 		// TODO Auto-generated method stub
 		String sql="delete from personcar where u_id=? and c_id=? and c_uid=? and p_state=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(perSonCar.getU_id());
 		params.add(perSonCar.getC_id());
@@ -253,19 +318,26 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		params.add(perSonCar.getP_state());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+/**
+ * 
+ * 检查是否还有与某车主编号关联的用户订单信息
+ * @return boolean
+ */
 	@Override
 	public boolean checkcuidperson(PerSonCar p) {
 		// TODO Auto-generated method stub
 		boolean flag=false;
 		String sql="select * from personcar where c_uid=?";
+			//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(p.getC_uid());
+		//获取结果集
 		ResultSet res=DbUtil.executeQuery(sql, params);
 		try {
 			while(res.next())
 			{
-				flag=true;
+				flag=true;  //若结果集存在，则返回为真
+				break;  //跳出循环
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -273,11 +345,15 @@ ResultSet res=		DbUtil.executeQuery(sql.toString(), params);
 		}
 		return flag;
 	}
-
+/**
+ * 根据车主编号删除用户订单信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletecuidperson(PerSonCar p) {
 		// TODO Auto-generated method stub
 		String sql="delete from personcar where c_uid=?";
+		//绑定参数
 		List<Object> params=new ArrayList<Object>();
 		params.add(p.getC_uid());
 		return DbUtil.executeUpdate(sql, params);
