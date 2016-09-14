a35369f4fdd299bb56e4f46d255fc2dffd606ee2
le15874811343 <576169235@qq.com>
2016-09-14 19:05:37 +0800

    用户信息服务接口

--- 后台/S4Manager/src/cn/com/service/IUserInfoService.java
+++ 后台/S4Manager/src/cn/com/service/IUserInfoService.java
@@ -5,12 +5,39 @@ import cn.com.bean.*;
 
 
 
-
+/**
+ * 用户信息服务接口
+ * @author
+ */
 public interface IUserInfoService {
+  /**
+   * 登录的服务
+   * @return UserInfo
+   */
 	public UserInfo login(UserInfo userInfo);
+  /**
+   * 按唯一条件获取用户信息的服务
+   *@return UserInfo 
+   */
 	public UserInfo getUserInfoByUnique(UserInfo userInfo);
+  /**
+   * 添加用户信息的服务
+   *@return boolean 
+   */
 	public boolean addUserInfo(UserInfo userInfo);
+  /**
+   * 修改用户信息的服务
+   *@return boolean 
+   */
 	public boolean updateUserInfo(UserInfo userInfo);
+  /**
+   * 修改用户密码的服务
+   *@return boolean 
+   */
 	public boolean updateUserPwd(UserInfo userInfo);
+  /**
+   * 删除用户信息的服务
+   *@return boolean 
+   */
 	 public boolean deleteuserinfouser(UserInfo u);
 }
