21d8d936841a65873d942954ef8289bc6849d0de
le15874811343 <576169235@qq.com>
2016-09-14 18:26:12 +0800

    品牌销量服务接口

--- 后台/S4Manager/src/cn/com/service/ICarchartService.java
+++ 后台/S4Manager/src/cn/com/service/ICarchartService.java
@@ -4,10 +4,29 @@ import java.util.List;
 import java.util.Map;
 
 import cn.com.bean.Carchart;
-
+/**
+ * 品牌销量服务接口
+ * 
+ */
 public interface ICarchartService {
+/**
+   * 按年月查询的服务
+   * 
+   */
 	public List<Carchart> chartinfo(int year,int month);
+/**
+ * 查询不同年的服务
+ * 
+ */
 	public List<Carchart> chartinfo();
+/**
+ * 按年月日查询的服务
+ * 
+ */
 	public Map<String, Carchart> chartinfo(int year, int month, int day);
+/**
+ * 按年查询的服务
+ * 
+ */
 	public Map<String, Carchart> chartinfo(int year);
 }
