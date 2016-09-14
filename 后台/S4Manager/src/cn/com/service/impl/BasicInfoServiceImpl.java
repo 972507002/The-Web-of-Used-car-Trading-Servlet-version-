1c4688b04f198ab0703c604ca03cad91adf4c0b1
le15874811343 <576169235@qq.com>
2016-09-14 19:32:33 +0800

    汽车基本信息服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/BasicInfoServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/BasicInfoServiceImpl.java
@@ -6,22 +6,35 @@ import cn.com.bean.*;
 
 import cn.com.dao.impl.*;
 import cn.com.service.*;
-
+/**
+ * 汽车基本信息服务实现类
+ * @author lej
+ */
 public class BasicInfoServiceImpl implements IBasicInfoService{
+	//汽车基础信息操作实现类的引用
 private BasicInfoDaoImpl basicInfoDaoImpl=new BasicInfoDaoImpl();
-
+/**
+ * 获取所有基本信息的服务
+ * @return Map<Long, BasicInfo>
+ */
 @Override
 public Map<Long, BasicInfo> getAllBasic() {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasic();
 }
-
+/**
+ *根据编号获取汽车基本信息的服务
+ *@return BasicInfo
+ */
 @Override
 public BasicInfo getAllBasicById(CarInfo carInfo) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasicById(carInfo);
 }
-
+/**
+ * 添加汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean addBasicInfo(BasicInfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -31,7 +44,10 @@ public boolean addBasicInfo(BasicInfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 修改汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean updateBasicInfo(BasicInfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -41,7 +57,10 @@ public boolean updateBasicInfo(BasicInfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 根据车主编号删除汽车基本信息的服务
+ *@return boolean  
+ */
 @Override
 public boolean deletebasicinfo(BasicInfo b) {
 	// TODO Auto-generated method stub
@@ -53,7 +72,10 @@ public boolean deletebasicinfo(BasicInfo b) {
 	
 	return flag;
 }
-
+/**
+ * 根据车编号删除汽车基本信息的服务
+ *@return boolean  
+ */
 @Override
 public boolean deletecidbasicinfo(BasicInfo b) {
 	// TODO Auto-generated method stub
@@ -65,13 +87,19 @@ public boolean deletecidbasicinfo(BasicInfo b) {
 	
 	return flag;
 }
-
+/**
+ * 检查是否还有与某车主编号关联的汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean checkbasicinfo(BasicInfo b) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.checkbasicinfo(b);
 }
-
+/**
+ * 检查是否还有与某车编号关联的汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean checkcidbasicinfo(BasicInfo b) {
 	// TODO Auto-generated method stub
