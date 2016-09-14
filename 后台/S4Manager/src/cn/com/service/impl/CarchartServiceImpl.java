b8c1e4eeaedb43d141ddd8ca997ce8811db9d3a2
le15874811343 <576169235@qq.com>
2016-09-14 19:49:43 +0800

    品牌销量服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/CarchartServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/CarchartServiceImpl.java
@@ -7,24 +7,44 @@ import cn.com.bean.Carchart;
 import cn.com.dao.ICarchartDao;
 import cn.com.dao.impl.CarchartDaoImpl;
 import cn.com.service.ICarchartService;
-
+/**
+ * 品牌销量服务实现类
+ * 
+ */
 public class CarchartServiceImpl implements ICarchartService{
-ICarchartDao carchartDao=new CarchartDaoImpl();
+	//品牌销量操作实现类的引用
+private  ICarchartDao carchartDao=new CarchartDaoImpl();
+  /**
+   * 按年月查询的服务
+   * 
+   */
 	@Override
 	public List<Carchart> chartinfo(int year, int month) {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo(year, month);
 	}
+/**
+ * 查询不同年的服务
+ * 
+ */
 	@Override
 	public List<Carchart> chartinfo() {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo();
 	}
+/**
+ * 按年月日查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo(year, month, day);
 	}
+/**
+ * 按年查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year) {
 		// TODO Auto-generated method stub
