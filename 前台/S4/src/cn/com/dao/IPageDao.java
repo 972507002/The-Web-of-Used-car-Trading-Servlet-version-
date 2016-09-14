a8169dabca5b843408d0bd09298e9bfd29cda8cf
le15874811343 <576169235@qq.com>
2016-09-14 13:40:51 +0800

    分页处理操作接口

--- 前台/S4/src/cn/com/dao/IPageDao.java
+++ 前台/S4/src/cn/com/dao/IPageDao.java
@@ -8,6 +8,12 @@ public interface IPageDao {
 
 	/**
 	 * 获取总记录条数
+	 * @parma minPrice   最小价格
+         * @parma maxPrice  最大价格
+         * @parma minDis   最小距离
+         * @parma maxDis  最大距离
+         * @parma minAge  最小车龄
+         * @parma maxAge  最大车龄
 	 * @return int
 	 */
 	public int queryMsgCount(Object object,int minPrice,int maxPrice,int minDis,int maxDis,int minAge,int maxAge);
@@ -16,6 +22,13 @@ public interface IPageDao {
 	 * 根据条件获取记录集合
 	 * @param curPage 当前页
 	 * @param rowsPrePage 每页面记录数
+	 * @parma minPrice   最小价格
+         * @parma maxPrice  最大价格
+         * @parma minDis   最小距离
+         * @parma maxDis  最大距离
+         * @parma minAge  最小车龄
+         * @parma maxAge  最大车龄
+         * @parma order 排序字段
 	 * @return Map<Integer,Object>
 	 */
 	public List<Object> showMsgInfoList(int curPage, int rowsPrePage,Object object,String order,int minPrice,int maxPrice,int minDis,int maxDis,int minAge,int maxAge);
