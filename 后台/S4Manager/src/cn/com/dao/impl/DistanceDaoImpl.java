34c118c7dea806aff6895e6bd9b11c29189ef9f9
le15874811343 <576169235@qq.com>
2016-09-14 15:37:24 +0800

    行驶距离操作实现类

--- 后台/S4Manager/src/cn/com/dao/impl/DistanceDaoImpl.java
+++ 后台/S4Manager/src/cn/com/dao/impl/DistanceDaoImpl.java
@@ -5,16 +5,24 @@ import java.util.*;
 import cn.com.bean.*;
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 行驶距离操作实现类
+ *@author lej 
+ */
 public class DistanceDaoImpl implements IDistanceDao,IPageDao {
 
-	
+   /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, Distance> distanceMap=new HashMap<Integer, Distance>();
 		StringBuffer sql=new StringBuffer("select * from(select rownum rn , b.* from");
 		sql.append("(select  a.* from distance  a order by d_count desc) b  where rownum<5)");
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), null);
 	try {
 		while(res.next()){
@@ -47,7 +55,10 @@ public class DistanceDaoImpl implements IDistanceDao,IPageDao {
 		return null;
 	}
 
-	
+    /**
+     * 获取行驶距离信息的记录总条数
+     * @return int
+     */	
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
@@ -55,7 +66,7 @@ int count=0;
 		
 		
 		StringBuffer sql=new StringBuffer("select count(*) from distance where 1=1");
-		
+		//获取结果集
 	ResultSet res=	DbUtil.executeQuery(sql.toString(), null);
 	try {
 		while(res.next()){
@@ -68,7 +79,12 @@ int count=0;
 		return count;
 	}
 
-	
+/**
+ * 分页获取行驶距离信息
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
@@ -77,6 +93,7 @@ int count=0;
 		 Map<Long, Object> brandMap=new HashMap<Long, Object>();
 			
 			sql.append(" order by d_count desc ) b  where rownum<=("+curPage+")*("+rowsPrePage+")) where rn>(("+curPage+")-1)*("+rowsPrePage+")");
+		    //获取结果集
 			ResultSet res=	 DbUtil.executeQuery(sql.toString(), null);
 			
 		    try {
@@ -94,46 +111,66 @@ int count=0;
 			}
 			return brandMap;
 	}
-	
+	/**
+           * 添加行驶距离的方法
+           * @parma distance
+           * @return int
+           */
 	@Override
 	public int addDistance(Distance distance) {
 		// TODO Auto-generated method stub
 		String sql="insert into distance values(seq_dis.nextval,?,?)";
+		//参数绑定
 		List<Object> params=new ArrayList<Object>();
 		
 		params.add(distance.getD_name());
 		params.add(distance.getD_count());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+          /**
+           * 删除行驶距离的方法
+           * @parma distance
+           * @return int
+           */
 	@Override
 	public int deleteDistance(Distance distance) {
 		// TODO Auto-generated method stub
+		//参数绑定
 		List<Object> params=new ArrayList<Object>();
 		String sql="delete from distance where d_id=?";
 		params.add(distance.getD_id());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+          /**
+           * 修改行驶距离的方法
+           * @parma distance
+           * @return int
+           */
 	@Override
 	public int updateDistance(Distance distance) {
 		// TODO Auto-generated method stub
 		List<Object> params=new ArrayList<Object>();
 		String sql="update distance set d_name=?,d_count=? where d_id=?";
-		
+		//参数绑定
 		params.add(distance.getD_name());
 		params.add(distance.getD_count());
 		params.add(distance.getD_id());
 		return DbUtil.executeUpdate(sql, params);
 	}
-
+    /**
+    * 
+    * 按编号获取行驶距离信息的方法
+    *@return Distance
+    */
 	@Override
 	public Distance getDistanceById(Distance distance) {
 		// TODO Auto-generated method stub
 		String sql="select * from distance where d_id=?";
 		Distance _Distance=null;
+		//参数绑定
 		List<Object> params=new ArrayList<Object>();
 		params.add(distance.getD_id());
+		//获取结果集
 		ResultSet res=	 DbUtil.executeQuery(sql.toString(), params);
 		
 	    try {
