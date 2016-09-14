86c2cb0eb5b7712d9af18b036f7712043a1a78b8
le15874811343 <576169235@qq.com>
2016-09-14 19:53:39 +0800

    行驶距离服务实现类

--- 后台/S4Manager/src/cn/com/service/impl/DistanceServiceImpl.java
+++ 后台/S4Manager/src/cn/com/service/impl/DistanceServiceImpl.java
@@ -6,9 +6,18 @@ import cn.com.bean.*;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 行驶距离服务实现类
+ *@author lej 
+ */
 public class DistanceServiceImpl implements IDistanceService,IPageDao {
+	//行驶距离操作实现类的引用
    private DistanceDaoImpl distanceDao=new DistanceDaoImpl();
-	
+   /**
+    * 
+    * 按热度获取行驶距离信息的服务
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
@@ -29,19 +38,32 @@ public class DistanceServiceImpl implements IDistanceService,IPageDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+    /**
+     * 获取行驶距离信息的记录总条数的服务
+     * @return int
+     */	
 	@Override
 	public int queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return distanceDao.queryPersonCarCount(object);
 	}
-	
+/**
+ * 分页获取行驶距离信息的服务
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return distanceDao.showPersonCarList(curPage, rowsPrePage, object);
 	}
+	  /**
+           * 添加行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	@Override
 	public boolean addDistance(Distance distance) {
 		// TODO Auto-generated method stub
@@ -51,6 +73,11 @@ public class DistanceServiceImpl implements IDistanceService,IPageDao {
 		}
 		return flag;
 	}
+	/**
+           * 删除行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	@Override
 	public boolean deleteDistance(Distance distance) {
 		// TODO Auto-generated method stub
@@ -60,6 +87,11 @@ public class DistanceServiceImpl implements IDistanceService,IPageDao {
 		}
 		return flag;
 	}
+	 /**
+           * 修改行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	@Override
 	public boolean updateDistance(Distance distance) {
 		// TODO Auto-generated method stub
@@ -69,7 +101,11 @@ public class DistanceServiceImpl implements IDistanceService,IPageDao {
 		}
 		return flag;
 	}
-
+   /**
+    * 
+    * 按编号获取行驶距离信息的服务
+    *@return Distance
+    */
 	@Override
 	public Distance getDistanceById(Distance distance) {
 		// TODO Auto-generated method stub
