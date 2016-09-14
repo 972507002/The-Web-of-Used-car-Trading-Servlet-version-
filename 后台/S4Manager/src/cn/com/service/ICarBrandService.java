18772c2f42bdc050803b258f5f2adf9605d0ef58
le15874811343 <576169235@qq.com>
2016-09-14 18:02:30 +0800

    品牌信息服务接口

--- 后台/S4Manager/src/cn/com/service/ICarBrandService.java
+++ 后台/S4Manager/src/cn/com/service/ICarBrandService.java
@@ -2,12 +2,40 @@ package cn.com.service;
 import cn.com.bean.*;
 
 import java.util.*;
-
+/**
+ * 品牌信息服务接口
+ * @author lej
+ */
 public interface ICarBrandService {
+  /**
+   * 按热度获取品牌信息的服务
+   * @return  Map<Integer, CarBrand>
+   */
 	public Map<Integer, CarBrand> getCarBrandByCount();
+  /**
+   * 获取所有品牌信息的服务
+   * @return Map<Integer,CarBrand>
+   */
 	public Map<Integer, CarBrand> getAllBrand();
+  /**
+   * 根据品牌编号获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	public CarBrand getBrandByID(CarBrand carBrand);
+  /**
+   * 添加品牌的服务
+   *@return int
+   */
 	 public boolean addCarBrand(CarBrand carBrand);
+  /**
+   * 修改品牌的服务
+   *@return int
+   */
 	  public boolean updateCarBrand(CarBrand carBrand);
+  /**
+   * 删除品牌的服务
+   *@return int
+   */
 	  public boolean deleteCarBrand(CarBrand carBrand);
 }
