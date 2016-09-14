12805f233ee64f7a0e3fe13e9e6290656b0c65c5
le15874811343 <576169235@qq.com>
2016-09-14 18:49:04 +0800

    营业额查询服务接口

--- 后台/S4Manager/src/cn/com/service/IPriceService.java
+++ 后台/S4Manager/src/cn/com/service/IPriceService.java
@@ -2,10 +2,29 @@ package cn.com.service;
 
 import java.util.List;
 import cn.com.bean.Price;
-
+/**
+ * 营业额查询服务接口
+ * 
+ */
 public interface IPriceService {
+	/**
+	 * 按年月日查询的服务
+	 * 
+	 */
 	public   List<Price> getsellinfo(int year,int month,int day);
+	/**
+	 * 按年月查询的服务
+	 * 
+	 */
 	public List<Price> getsellinfo(int year,int month);
+	/**
+	 * 按年查询的服务
+	 * 
+	 */
 	public  List<Price> getsellinfo(int year);
+	/**
+	 * 查询不同年营业额的服务
+	 * 
+	 */
 	public List<Price> getsellinfo();
 }
