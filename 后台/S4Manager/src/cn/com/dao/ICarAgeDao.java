c9b49daa5e03bd8a4f48f04b37f779fc8a4244dc
le15874811343 <576169235@qq.com>
2016-09-14 13:10:19 +0800

    车龄信息操作接口

--- 后台/S4Manager/src/cn/com/dao/ICarAgeDao.java
+++ 后台/S4Manager/src/cn/com/dao/ICarAgeDao.java
@@ -2,10 +2,34 @@ package cn.com.dao;
 import java.util.Map;
 
 import cn.com.bean.*;
+/**
+ * 车龄信息操作接口
+ *@author  lej 
+ */
 public interface ICarAgeDao {
+	/**
+	 * 按热度获取车龄信息的方法
+	 * @return Map<Integer,CarAge>
+	 */
 	public Map<Integer,CarAge> getCarAgeByCount();
+	/**
+	 * 添加车龄信息的方法
+	 * @return   int
+	 */
 	public int addCarAge(CarAge carAge);
+	/**
+	 * 删除车龄信息的方法
+	 * @return   int
+	 */
 	public int deleteCarAge(CarAge carAge);
+	/**
+	 * 修改车龄信息的方法
+	 * @return   int
+	 */
 	public int updateCarAge(CarAge carAge);
+	/**
+	 * 获取车龄信息的方法
+	 * @return Map<Integer,CarAge>
+	 */
 	public CarAge getCarAgeById(CarAge carAge);
 }
